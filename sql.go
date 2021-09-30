bbb8e2935f16e8752e41c90565d50c785505f327
mrod502 <mrod22502@gmail.com>
2021-09-29 20:38:36 -0400

    remove unusaed

--- sql.go
+++
@@ -1,6 +0,0 @@
-package hitbtc
-
-const (
-	//CreateTableTicker - query to create table in database for HitBTC ticker
-	CreateTableTicker = `CREATE TABLE IF NOT EXISTS hitbtc_ticker (TickerID VARCHAR(20) NOT NULL PRIMARY KEY,Symbol VARCHAR(20),Ask DECIMAL,Bid DECIMAL,"Last" DECIMAL,"Open" DECIMAL,Low DECIMAL,High DECIMAL,Volume DECIMAL,VolumeQuote DECIMAL,"Timestamp" TIMESTAMP);`
-)
